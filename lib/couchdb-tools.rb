bab54ba63b0dfd4c6bcf1460b496f83c26ec452f
Chris Gallimore <chris@slice-dice.com>
2013-02-24 13:23:55 -0800

    fix indentation

--- lib/couchdb-tools.rb
+++ lib/couchdb-tools.rb
@@ -11,17 +11,17 @@ Dir[File.join(File.dirname(__FILE__), "**", "*.rb")].each {|file| require file}
 
 module CouchDBTools
 
-	def self.start(opts)
-		DBLogger.config
+  def self.start(opts)
+    DBLogger.config
 
-		begin
-			EM.run do
-			  Scheduler	
-			end
+      begin
+        EM.run do
+	  Scheduler	
+	end
 
-    rescue => e
-      DBLogger.log.fatal "Monitor failed with the following issue #{e.message}"
-    end
+      rescue => e
+        DBLogger.log.fatal "Monitor failed with the following issue #{e.message}"
+      end
   end
 
 end
