4d3489442519074480e9a8f9bb995245cd70a8e3
Chris Gallimore <chris@slice-dice.com>
2013-02-24 13:16:50 -0800

    logger class

---
+++ lib/couchdb-tools/logger.rb
@@ -0,0 +1,27 @@
+module CouchDBTools
+  class DBLogger
+
+    def self.config(options = {})
+      file = options['file']
+      level = options['level']
+      rotation = options['rotation']
+
+      file ||= STDOUT
+      level ||= 'info'
+      rotation ||= 'daily'
+
+      @@log = Logger.new(file, rotation)
+      @@log.level = Logger.const_get(level.upcase.to_sym)
+
+      @@log.formatter = proc do |severity, datetime, progname, msg|
+       "#{datetime}: [#{severity}] : #{msg}\n"
+      end
+      @@log.info "Logger started with a level of #{log_level.upcase}."
+    end
+
+    def self.log
+      @@log
+    end
+
+  end
+end
